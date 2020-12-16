be09875e42c3d45c74ea42d00ca3821aa39085dd
RuuuFF <ruuufffaria@gmail.com>
2020-12-16 15:37:30 -0300

    comentários

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const labels = document.querySelectorAll('.form-control label');
 labels.forEach(label => {
   label.innerHTML = label.innerText
     .split('')
-    .map((letter, index) => `<span style="transition: ${index * 100}ms">${letter}</span>`)
+    .map((letter, index) => `<span style="transition-delay: ${index * 40}ms">${letter}</span>`)
     .join('');
   
     /* da o foco no input caso clique em cima do label */
@@ -11,3 +11,8 @@ labels.forEach(label => {
       label.previousElementSibling.focus();
     });
 });
+
+// innerHTML, innerText: https://cutt.ly/ZhJPk4c
+// Split: https://www.w3schools.com/jsref/jsref_split.asp
+// Map: https://www.w3schools.com/jsref/jsref_map.asp
+// Join: https://www.w3schools.com/jsref/jsref_join.asp
